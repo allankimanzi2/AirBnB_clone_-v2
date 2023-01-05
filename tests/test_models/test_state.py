bad132143551d9c61905f2e7ff85191bd0983025
allankimanzi2 <allankim457@gmail.com>
2023-01-05 13:22:17 +0000

    airbnb clone 2

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,15 +5,15 @@ from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    ""states test class" """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """state test class init """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """testing state name attr """
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,30 +5,30 @@ from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """test class for user model """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """user test class init """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """testing user first anme attr """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """testing user last name attr """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """testing user email attr """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """testing user password attr """
         new = self.value()
         self.assertEqual(type(new.password), str)
